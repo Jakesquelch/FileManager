4c38345c96dbc19039de600c17c6c6c52618f1eb
Jake Squelch <jakewsquelch@gmail.com>
2025-04-10 23:04:16 +0100

    Added some comments

--- FileManager.cpp
+++ FileManager.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <filesystem>
+#include <filesystem> //allows working with files and directories
 #include "FileManager.hpp"
 
-namespace fs = std::filesystem; //can write fs instead of std::filesystem everytime
+namespace fs = std::filesystem; //so can write fs instead of std::filesystem 
 
 std::string listFiles(std::string directory)
 {
 	std::string result;
 
 	try {
+		//iterate over files/directories in the specified directory & append their paths (converted to strings) to result
 		for (const auto& entry : fs::directory_iterator(directory)) {
 			result += entry.path().string() + "\n";
 		}
-	}
+	} 
 	catch (const fs::filesystem_error& e) {
 		result = "Error accessing directory: " + std::string(e.what()) + "\n";
 	}
