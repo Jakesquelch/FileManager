a9765248eac3ef90fed0c281ed533d2f9c3204f6
Jake Squelch <jakewsquelch@gmail.com>
2025-04-11 16:02:12 +0100

    Began developing delete part

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,14 @@ int main()
 {
     int choice;
     do {
-        std::cout << "1. List files\n2. Create file\n3. Exit\nChoice: ";
+        std::cout << "1. List files\n2. Create file\n3. Delete a file\n4. Exit\nChoice: ";
         std::cin >> choice;
 
         switch (choice) {
         case 1: {//list files
             std::string output = listFiles(".");
             std::cout << "\n(1) Here are the listed files:\n";
-            std::cout << output <<"\n";
+            std::cout << output << "\n";
             break;
         }
         case 2: {//create files
@@ -20,20 +20,24 @@ int main()
             std::cin >> filename;
 
             if (createFile(filename)) {
-                std::cout << "File created successfully: " << filename << "\n\n";
+                std::cout << "File created successfully!\n\n";
             }
             else {
                 std::cout << "Error occured creating file\n\n";
             }
             break;
         }
-            case 3:
+        case 3: {//delete a file
+            std::cout << "\n(3) Successfully deleted a file\n\n";
+            break;
+        }
+            case 4:
             std::cout << "Goodbye mate!\n";
             break;
             default:
             std::cout << "Invalid choice\n";
         }
-    } while (choice != 3);
+    } while (choice != 4);
 
     return 0;
 }
